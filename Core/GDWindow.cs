5d7b38a011fd7c5d00a5f2b2abc80df186eae965
Fra <Fra@DESKTOP-N89V42E>
2025-04-16 15:32:44 +0200

    Added Delta Frame GDWindow

--- Core/GDWindow.cs
+++ Core/GDWindow.cs
@@ -4,6 +4,19 @@ using Raylib_cs;
 namespace GDraw.Core {
 	public class GDWindow
 	{
+		public static class Time
+		{
+			public static float Delta { get; private set; }
+			private static float lastFrame;
+			
+			public static void Update()
+			{
+				float currentFrame = Raylib.GetFPS() / 1000.0f;
+				Delta = currentFrame - lastFrame;
+				lastFrame = currentFrame;
+			}
+
+		}
 		public static void Init(int width, int height, string title)
 		{
 			Raylib.InitWindow(width, height, title);
---
+++ README.md
@@ -0,0 +1,10 @@
+# GDraw
+**GDraw Is Framework To create 2D and 3D Games** 
+
+## Features 
+- [x] 2D
+- [ ] 3D
+- [ ] Shader
+- [ ] Input 
+
+# How to Add GDraw in my Project? 
